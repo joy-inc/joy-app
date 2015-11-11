945b6a8534eef73564bc4f4048a3df4d0719e3e9
shiwen.dai <shiwen.dai@qyer.com>
2015-11-11 15:52:11 +0800

    整理LogMgr相关代码

--- app/src/main/java/com/joy/app/JoyApplication.java
+++ app/src/main/java/com/joy/app/JoyApplication.java
@@ -3,6 +3,7 @@ package com.joy.app;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.joy.app.utils.prefs.CommonPrefs;
 import com.joy.library.BaseApplication;
+import com.joy.library.utils.LogMgr;
 
 /**
  * Created by KEVIN.DAI on 15/7/8.
@@ -19,6 +20,13 @@ public class JoyApplication extends BaseApplication {
     private void initApplication() {
 
         Fresco.initialize(getContext());
+        if (BuildConfig.DEBUG) {
+
+            LogMgr.turnOn();
+        } else {
+
+            LogMgr.turnOff();
+        }
     }
 
     public static void releaseForExitApp() {
@@ -35,18 +43,21 @@ public class JoyApplication extends BaseApplication {
 
     /**
      * 用户是否登录
+     *
      * @return
      */
-    public static boolean isLogin(){
+    public static boolean isLogin() {
 
         return false;
     }
 
     /**
      * 直接获取用户token
+     *
      * @return
      */
-    public static String getUserToken(){
+    public static String getUserToken() {
+
         return "";
     }
 }
