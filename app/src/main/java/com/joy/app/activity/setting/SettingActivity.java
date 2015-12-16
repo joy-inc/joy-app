cd07da8b02942a7b7f374b0593735346ca4a9881
liulongzhenhai <longzhenhai.liu@qyer.com>
2015-12-16 12:25:12 +0800

    如果是测试token.退出直接退出

--- app/src/main/java/com/joy/app/activity/setting/SettingActivity.java
+++ app/src/main/java/com/joy/app/activity/setting/SettingActivity.java
@@ -224,12 +224,12 @@ public class SettingActivity extends BaseHttpUiActivity<String> implements View.
     private void loginOut() {
 
         //网络请求,发广播
+
+        ObjectRequest req = ReqFactory.newPost(UserHtpUtil.URL_USER_LOGIN_OUT, String.class, UserHtpUtil.userLoginOut(JoyApplication.getUserToken()));
         if (BuildConfig.DEBUG && ("41dd399909650830414ae7b0276d8dc2ae777fa2d552c5a4dd93fcd0040bce1e".equals(JoyApplication.getUserToken())
                 || "e4f6ed7c3acb5bbcd17f62f82a0effb22bc3c1b319b50f825a95dc3891af0aeb".equals(JoyApplication.getUserToken()))){
-            ToastUtil.showToast("退毛线啊退,再退后台要吵着跳楼了");
-            return;
+            req.setData("");
         }
-        ObjectRequest req = ReqFactory.newPost(UserHtpUtil.URL_USER_LOGIN_OUT, String.class, UserHtpUtil.userLoginOut(JoyApplication.getUserToken()));
         req.setResponseListener(new ObjectResponse() {
 
             @Override
